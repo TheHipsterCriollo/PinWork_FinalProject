897f2a66a1d8631e67b3e6f0eb46011ab76eb172
Sebastián Gaviria <vagyriag@hotmail.com>
2017-11-01 17:02:18 -0500

    episodios

--- src/Episodios.tsx
+++ src/Episodios.tsx
@@ -1,29 +1,38 @@
 import * as React from 'react';
 
 export class Episodios extends React.Component<any, any> {
-  
+  state = {
+    episodios: []
+  }
+
   componentWillMount(){
     this.getEpisodios();
   }
   
   getEpisodios(){
-    fetch('http://api.tvmaze.com/shows/1871/episodes')
+    fetch('http://api.tvmaze.com/shows/1128/episodes')
       .then(data => data.json())
       .then(data => {
         console.log(data)
         this.setState({
-          serie: {
-            nombre: data.name,
-            resumen: data.summary,
-            img: data.image.medium
-          }
+          episodios: data
         })
       });
   }
   
   render() {
     return <div className="row">
-      episodios
+      {this.state.episodios.map(({ name, url, image, id }) => 
+        <div className="col-sm-3" key={id}>
+          <div className="card mb-4">
+            {image && <img className="card-img-top" src={image.medium} />}
+            <div className="card-body">
+              <h4 className="card-title">{name}</h4>
+              <a href={url} target="_blank" className="btn btn-info">ver</a>
+            </div>
+          </div>
+        </div>
+      )}
     </div>; 
   }
 }
--- src/Root.tsx
+++ src/Root.tsx
@@ -1,78 +1,70 @@
 import * as React from 'react';
 
 import { General } from './General';
+import { Episodios } from './Episodios';
 
 export class Root extends React.Component<any,any>{
   
-    state = {
-      serie: {
-        nombre: '',
-        resumen: '',
-        img: '',
-      },
-      activo: 'General'
-    }
+  state = {
+    serie: {
+      nombre: '',
+      resumen: '',
+      img: '',
+    },
+    activo: 'General'
+  }
   
-    componentWillMount(){
-      this.getGeneral();
-    }
-
-    getGeneral(){
-      fetch('http://api.tvmaze.com/shows/1871')
-        .then(data => data.json())
-        .then(data => {
-          console.log(data)
-          this.setState({
-            serie: {
-              nombre: data.name,
-              resumen: data.summary,
-              img: data.image.medium
-            }
-          })
-        });
-    }
-
+  componentWillMount(){
+    this.getGeneral();
+  }
+  
+  getGeneral(){
+    fetch('http://api.tvmaze.com/shows/1128')
+    .then(data => data.json())
+    .then(data => {
+      console.log(data)
+      this.setState({
+        serie: {
+          nombre: data.name,
+          resumen: data.summary,
+          img: data.image.original
+        }
+      })
+    });
+  }
   
   
-    render(){
-      const { activo } = this.state
-      const { nombre, resumen, img } = this.state.serie;
-      
-      const tabs = ['General', 'Episodios', 'Reparto'];
-      
-      return <div>
-         <div className="container">
-           <div className="row">
-            <div className="col-sm-12">
-              <h2>{nombre}</h2>
-              
-              <ul className="nav nav-tabs">
-               {tabs.map((tab, i) => 
-                  <li className="nav-item" key={tab}>
-                    <a className={`nav-link ${activo == tab && 'active'}`} 
-                    href="#"
-                    onClick={() => this.setState({ activo: tab })}>{tab}</a>
-                  </li>
-                )}
-              </ul>
-            </div>
-           </div>
-           
-           {/* comentario */}
-           
-           {activo == 'General' && <General img={img} resumen={resumen} />}
-           {activo == 'Episodios'  && <Episodios />}
+  
+  render(){
+    const { activo } = this.state
+    const { nombre, resumen, img } = this.state.serie;
+    
+    const tabs = ['General', 'Episodios', 'Reparto'];
+    
+    return <div>
+      <div className="container">
+        <div className="row" style={{ marginBottom: 20 }}>
+          <div className="col-sm-12">
+            <h2 className="display-3 my-5">{nombre}</h2>
             
-         </div>
-      </div>;
-    }
+            <ul className="nav nav-tabs">
+              {tabs.map((tab, i) => 
+                <li className="nav-item" key={tab}>
+                <a className={`nav-link ${activo == tab && 'active'}`} 
+                href="#"
+                onClick={() => this.setState({ activo: tab })}>{tab}</a>
+                </li>
+              )}
+            </ul>
+          </div>
+        </div>
+        
+        {/* comentario */}
+        
+        {activo == 'General' && <General img={img} resumen={resumen} />}
+        {activo == 'Episodios'  && <Episodios />}
+        
+      </div>
+    </div>;
+  }
 }
-
-
-
-
-
-
-
-
-
