1e4aa36d7291b9e703f84a97124cdf4d6b9862d7
Sebastian Gaviria Giraldo <1112478378@academico.icesi.edu.co>
2017-11-10 19:14:24 -0500

    oks

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,70 +1,80 @@
 import * as React from 'react';
 
-import { General } from './General';
-import { Episodios } from './Episodios';
-
 export class Root extends React.Component<any,any>{
-  
-  state = {
-    serie: {
-      nombre: '',
-      resumen: '',
-      img: '',
-    },
-    activo: 'General'
-  }
-  
-  componentWillMount(){
-    this.getGeneral();
-  }
-  
-  getGeneral(){
-    fetch('http://api.tvmaze.com/shows/1128')
-    .then(data => data.json())
-    .then(data => {
-      console.log(data)
-      this.setState({
-        serie: {
-          nombre: data.name,
-          resumen: data.summary,
-          img: data.image.original
-        }
-      })
-    });
-  }
-  
-  
-  
+    state = {
+        projects: [],
+    };
+
+    componentWillMount(){
+        this.getProjects();
+    }
+    
+    getProjects(){
+        fetch('https://api.behance.net/v2/projects?q=motorcycle&client_id=UqSBClMILi0xJ2QvW7dPM5ppzqIpybVh', {
+            method: 'get',
+            mode: 'no-cors'
+        })
+            .then((e) => e.json())
+            .then((info) => {
+                this.setState({
+                    projects: info.projects
+                });
+            })
+    }
+    
   render(){
-    const { activo } = this.state
-    const { nombre, resumen, img } = this.state.serie;
-    
-    const tabs = ['General', 'Episodios', 'Reparto'];
-    
     return <div>
-      <div className="container">
-        <div className="row" style={{ marginBottom: 20 }}>
-          <div className="col-sm-12">
-            <h2 className="display-3 my-5">{nombre}</h2>
-            
-            <ul className="nav nav-tabs">
-              {tabs.map((tab, i) => 
-                <li className="nav-item" key={tab}>
-                <a className={`nav-link ${activo == tab && 'active'}`} 
-                href="#"
-                onClick={() => this.setState({ activo: tab })}>{tab}</a>
-                </li>
-              )}
-            </ul>
-          </div>
+        <Header imagen="https://compass-ssl.xbox.com/assets/57/2d/572dc161-0e1c-4ff2-ae9f-bfc4d03ecb08.jpg?n=Cuphead_GLP-Page-Hero-1084_1920x600.jpg">
+            <h1>Showcase & Discover <br /> Creative Work</h1>
+        </Header>
+        
+        <div className="row">
+            {this.state.projects.map((obj, index) => 
+                <Preview key={index}
+                    imagen={obj.covers['115']}
+                    titulo={obj.name}
+                    texto={obj.fields[0]}
+                    btn={'go'}
+                    />
+            )}
         </div>
         
-        {/* comentario */}
-        
-        {activo == 'General' && <General img={img} resumen={resumen} />}
-        {activo == 'Episodios'  && <Episodios />}
-        
-      </div>
     </div>;
   }
 }
+
+
+
+export class Header extends React.Component<any,any>{
+  render(){
+    return <div style={{
+            background: 'url('+this.props.imagen+')',
+            backgroundSize: 'cover',
+            textAlign: 'center',
+            color: 'red'
+        }}>
+        <h1 style={{ whiteSpace: 'pre' }}>{this.props.titulo}</h1>
+        {this.props.children}
+    </div>;
+  }
+}
+
+
+export class Preview extends React.Component<any,any>{
+  render(){
+    return <div className="col-sm-4">
+        <div className="card">
+            <img src={this.props.imagen} className="card-img-top" />
+            <div className="card-body">
+                <h4 className="card-title">{this.props.titulo}</h4>
+                <p className="card-text">{this.props.texto}</p>
+                <a href="#" className="btn btn-primary">{this.props.btn}</a>
+              </div>
+            
+        </div>
+    </div>;
+  }
+}
+
+
+
